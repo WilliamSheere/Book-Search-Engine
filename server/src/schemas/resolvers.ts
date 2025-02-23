159b7d7d973fab6e51b6e82e599da2ae81a7d385
William Sheere IV <Sheerebilly@yahoo.com>
2025-02-23 17:40:53 -0500

    backend completed

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,88 +1,95 @@
-import  User from '../models/index.js';
-import { signToken, AuthenticationError } from '../services/auth.js'; 
+import User from "../models/index.js";
+import { signToken, AuthenticationError } from "../services/auth.js";
 
 // Define types for the arguments
 interface AddUserArgs {
-  input:{
-    username: string;
-    email: string;
-    password: string;
-  }
+	input: {
+		username: string;
+		email: string;
+		password: string;
+	};
 }
 
 interface LoginUserArgs {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 // interface UserArgs {
 //   username: string;
 // }
 
-
 const resolvers = {
-  Query: {
-    // Query to get the authenticated user's information
-    // The 'me' query relies on the context to check if the user is authenticated
-    me: async (_parent: any, _args: any, context: any) => {
-      // If the user is authenticated, find and return the user's information along with their thoughts
-      if (context.user) {
-        return User.findOne({ _id: context.user._id });
-      }
-      // If the user is not authenticated, throw an AuthenticationError
-      throw new AuthenticationError('Could not authenticate user.');
-    },
-  },
-  Mutation: {
-    addUser: async (_parent: any, { input }: AddUserArgs) => {
-      // Create a new user with the provided username, email, and password
-      const user = await User.create({ ...input });
-    
-      // Sign a token with the user's information
-      const token = signToken(user.username, user.email, user._id);
-    
-      // Return the token and the user
-      return { token, user };
-    },
-    
-    login: async (_parent: any, { email, password }: LoginUserArgs) => {
-      // Find a user with the provided email
-      const user = await User.findOne({ email });
-    
-      // If no user is found, throw an AuthenticationError
-      if (!user) {
-        throw new AuthenticationError('Could not authenticate user.');
-      }
-    
-      // Check if the provided password is correct
-      const correctPw = await user.isCorrectPassword(password);
-    
-      // If the password is incorrect, throw an AuthenticationError
-      if (!correctPw) {
-        throw new AuthenticationError('Could not authenticate user.');
-      }
-    
-      // Sign a token with the user's information
-      const token = signToken(user.username, user.email, user._id);
-    
-      // Return the token and the user
-      return { token, user };
-    },
-    // addThought: async (_parent: any, { input }: AddThoughtArgs, context: any) => {
-    //   if (context.user) {
-    //     const thought = await Thought.create({ ...input });
+	Query: {
+		// Query to get the authenticated user's information
+		// The 'me' query relies on the context to check if the user is authenticated
+		me: async (_parent: any, _args: any, context: any) => {
+			// If the user is authenticated, find and return the user's information along with their thoughts
+			if (context.user) {
+				return User.findOne({ _id: context.user._id });
+			}
+			// If the user is not authenticated, throw an AuthenticationError
+			throw new AuthenticationError("Could not authenticate user.");
+		},
+	},
+	Mutation: {
+		addUser: async (_parent: any, { input }: AddUserArgs) => {
+			// Create a new user with the provided username, email, and password
+			const user = await User.create({ ...input });
 
-    //     await User.findOneAndUpdate(
-    //       { _id: context.user._id },
-    //       { $addToSet: { thoughts: thought._id } }
-    //     );
+			// Sign a token with the user's information
+			const token = signToken(user.username, user.email, user._id);
 
-    //     return thought;
-    //   }
-    //   throw AuthenticationError;
-    //   ('You need to be logged in!');
-    // }
-  },
+			// Return the token and the user
+			return { token, user };
+		},
+
+		login: async (_parent: any, { email, password }: LoginUserArgs) => {
+			// Find a user with the provided email
+			const user = await User.findOne({ email });
+
+			// If no user is found, throw an AuthenticationError
+			if (!user) {
+				throw new AuthenticationError("Could not authenticate user.");
+			}
+
+			// Check if the provided password is correct
+			const correctPw = await user.isCorrectPassword(password);
+
+			// If the password is incorrect, throw an AuthenticationError
+			if (!correctPw) {
+				throw new AuthenticationError("Could not authenticate user.");
+			}
+
+			// Sign a token with the user's information
+			const token = signToken(user.username, user.email, user._id);
+
+			// Return the token and the user
+			return { token, user };
+		},
+		addBook: async (_parent: any, { book }: any, context: any) => {
+			if (context.user) {
+				const user = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $addToSet: { savedBooks: book } }
+				);
+
+				return user;
+			}
+			throw new AuthenticationError("You need to be logged in!");
+		},
+		deleteBook: async (_parent: any, { bookId }: any, context: any) => {
+			if (context.user) {
+				const user = await User.findOneAndUpdate(
+					{ _id: context.user._id },
+					{ $pull: { savedBooks: { bookId } } }
+				);
+
+				return user;
+			}
+			throw new AuthenticationError("You need to be logged in!");
+		},
+	},
 };
 
 export default resolvers;
